ea7f621c1c5c8579823547bb4d6e65348d2601f2
Gobinder2901 <dhillongobinder@gmail.com>
2026-04-26 04:54:57 -0600

    Update vite.config.js

--- qrcafe/vite.config.js
+++ qrcafe/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 57330,
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html'
     }
+  }
 })
